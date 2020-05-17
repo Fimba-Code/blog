881bf69bb078cc6e1ef19f0ee9d1311ebf33b5cd
José Cage <jose.cage@doesangue.me>
2020-05-17 21:34:59 +0100

    Added gatsby feed rss support

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,5 +85,58 @@ module.exports = {
         ignore: [`**/styles.(js)?(x)`],
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                url
+                site_url: url
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.url + edge.node.frontmatter.slug,
+                  guid: site.siteMetadata.url + edge.node.frontmatter.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html                      
+                      frontmatter {
+                        title
+                        date
+                        path
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/feed.xml",
+            title: "Fimba Code RSS Feed",
+          },
+        ],
+      },
+    },
   ],
 }
